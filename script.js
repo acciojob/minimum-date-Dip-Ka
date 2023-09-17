6ac566b2344e2d016e22ae25c044af05f9e034d5
Dip-Ka <143940154+Dip-Ka@users.noreply.github.com>
2023-09-17 11:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,66 @@
-function minDate(dates) {
-  //write you code here
-}
+  <script>
+		// Define the function to find the minimum date
+		function minDate(dates) {
+			// Check if the array is empty
+			if (dates.length === 0) {
+				return null;
+			}
 
-// Do not change the code
+			// Initialize the minimum date with the first date in the array
+			let min = new Date(dates[0]);
 
-var dates = [
-  "2023/03/01",
-  "2023/03/02",
-  "2023/03/03",
-  "2023/03/04",
-  "2023/03/05",
-  "2023/03/06",
-  "2023/03/07",
-  "2023/03/08",
-  "2023/03/09",
-  "2023/03/10",
-  "2023/03/11",
-  "2023/03/12",
-  "2023/03/13",
-  "2023/03/14",
-  "2023/03/15",
-  "2023/03/16",
-  "2023/03/17",
-  "2023/03/18",
-  "2023/03/19",
-  "2023/03/20",
-  "2023/03/21",
-  "2023/03/22",
-  "2023/03/23",
-  "2023/03/24",
-  "2023/03/25",
-  "2023/03/26",
-  "2023/03/27",
-  "2023/03/28",
-  "2023/03/29",
-  "2023/03/30",
-];
+			// Loop through the array to find the minimum date
+			for (let i = 1; i < dates.length; i++) {
+				const currentDate = new Date(dates[i]);
 
-alert(minDate(dates));
+				// Compare the current date with the minimum date
+				if (currentDate < min) {
+					min = currentDate;
+				}
+			}
+
+			// Format the minimum date as "YYYY/MM/DD" and return it as a string
+			const year = min.getFullYear();
+			const month = String(min.getMonth() + 1).padStart(2, '0');
+			const day = String(min.getDate()).padStart(2, '0');
+
+			return `${year}/${month}/${day}`;
+		}
+
+		// Do not change the code
+
+		var dates = [
+			"2023/03/01",
+			"2023/03/02",
+			"2023/03/03",
+			"2023/03/04",
+			"2023/03/05",
+			"2023/03/06",
+			"2023/03/07",
+			"2023/03/08",
+			"2023/03/09",
+			"2023/03/10",
+			"2023/03/11",
+			"2023/03/12",
+			"2023/03/13",
+			"2023/03/14",
+			"2023/03/15",
+			"2023/03/16",
+			"2023/03/17",
+			"2023/03/18",
+			"2023/03/19",
+			"2023/03/20",
+			"2023/03/21",
+			"2023/03/22",
+			"2023/03/23",
+			"2023/03/24",
+			"2023/03/25",
+			"2023/03/26",
+			"2023/03/27",
+			"2023/03/28",
+			"2023/03/29",
+			"2023/03/30",
+		];
+
+		alert(minDate(dates));
+	</script>
